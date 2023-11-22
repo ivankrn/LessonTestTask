74618c7f68ccff941641e34c5c505c8963767139
Ivan Kropotin <kropotiniv@yandex.ru>
2023-11-22 17:34:34 +0500

    1) Упростил FakeBot 2) Перенес инициализацию bot в метод @Before 3) Исправил тесты

--- src/test/java/example/bot/BotLogicTest.java
+++ src/test/java/example/bot/BotLogicTest.java
@@ -3,8 +3,6 @@ package example.bot;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.List;
-
 import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -12,213 +10,84 @@ public class BotLogicTest {
 
     private BotLogic botLogic;
     private User user;
-    private final FakeBot bot = new FakeBot();
+    private FakeBot bot;
 
     @Before
     public void setUp() {
         user = new User(1L);
+        bot = new FakeBot();
         botLogic = new BotLogic(bot);
     }
 
     /**
-     * Если пользователь присылает команду /test, то его состояние должно стать {@link State#TEST State.TEST} и бот
-     * должен отправить первый вопрос
+     * Проверяет команду /test при правильных ответах пользователя на вопросы
      */
     @Test
-    public void whenTestCommand_thenSetUserStateToTestAndSendFirstQuestion() {
+    public void givenUserAnswersCorrect_whenTestCommand() {
         botLogic.processCommand(user, "/test");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
         assertEquals(State.TEST, user.getState());
-        assertEquals("Вычислите степень: 10^2", chatMessages.get(0));
-    }
+        assertEquals("Вычислите степень: 10^2", bot.getMessages().get(0));
 
-    /**
-     * Если во время теста пользователь отвечает на последний вопрос, то состояние пользователя должно стать
-     * {@link State#INIT State.INIT} и бот должен отправить сообщение о том, что тест завершен
-     */
-    @Test
-    public void givenUserInTestState_whenUserAnswersLastQuestion_thenSetUserStateToInitAndSendStopMessage() {
-        botLogic.processCommand(user, "/test");
         botLogic.processCommand(user, "100");
+        assertEquals("Правильный ответ!", bot.getMessages().get(1));
+        assertEquals("Сколько будет 2 + 2 * 2", bot.getMessages().get(2));
 
         botLogic.processCommand(user, "6");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
-        assertEquals(State.INIT, user.getState());
-        assertEquals("Тест завершен", chatMessages.get(4));
-    }
-
-    /**
-     * Если во время теста пользователь отвечает на непоследний вопрос, то бот должен отправить сообщение со следующим
-     * вопросом
-     */
-    @Test
-    public void givenUserInTestState_whenUserAnswers_thenSendNextQuestion() {
-        botLogic.processCommand(user, "/test");
-
-        botLogic.processCommand(user, "100");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
-        assertEquals("Сколько будет 2 + 2 * 2", chatMessages.get(2));
-    }
-
-    /**
-     * Если во время теста пользователь правильно отвечает на вопрос, то бот должен отправить сообщение о том, что
-     * ответ правильный
-     */
-    @Test
-    public void givenUserInTestState_whenUserAnswersCorrect_thenSendApprove() {
-        botLogic.processCommand(user, "/test");
-
-        botLogic.processCommand(user, "100");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
-        assertEquals("Правильный ответ!", chatMessages.get(1));
-    }
-
-    /**
-     * Если во время теста пользователь неправильно отвечает на вопрос, то бот должен отправить сообщение о том, что
-     * ответ неправильный и указать верный ответ
-     */
-    @Test
-    public void givenUserInTestState_whenUserAnswersWrong_thenSendWrongAnswer() {
-        botLogic.processCommand(user, "/test");
-
-        botLogic.processCommand(user, "10");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
-        assertEquals("Вы ошиблись, верный ответ: 100", chatMessages.get(1));
-    }
-
-    /**
-     * Если во время теста пользователь отвечает правильно, то список неправильных ответов на вопросы пользователя
-     * не должен измениться
-     */
-    @Test
-    public void givenUserInTestState_whenUserAnswersCorrect_thenDoNotAddWrongAnswer() {
-        botLogic.processCommand(user, "/test");
-
-        botLogic.processCommand(user, "100");
-
+        assertEquals("Правильный ответ!", bot.getMessages().get(3));
         assertTrue(user.getWrongAnswerQuestions().isEmpty());
+        assertEquals(State.INIT, user.getState());
+        assertEquals("Тест завершен", bot.getMessages().get(4));
     }
 
     /**
-     * Если во время теста пользователь отвечает неправильно, то в список неправильных ответов на вопросы пользователя
-     * должен добавиться неправильно отвеченный вопрос
+     * Проверяет команду /test при неправильных ответах пользователя на вопросы
      */
     @Test
-    public void givenUserInTestState_whenUserAnswersWrong_thenAddWrongAnswer() {
+    public void givenUserAnswersWrong_whenTestCommand() {
         botLogic.processCommand(user, "/test");
+        assertEquals(State.TEST, user.getState());
+        assertEquals("Вычислите степень: 10^2", bot.getMessages().get(0));
 
         botLogic.processCommand(user, "10");
-
+        assertEquals("Вы ошиблись, верный ответ: 100", bot.getMessages().get(1));
+        assertEquals(1, user.getWrongAnswerQuestions().size());
         assertEquals("Вычислите степень: 10^2", user.getWrongAnswerQuestions().get(0).getText());
-    }
+        assertEquals("Сколько будет 2 + 2 * 2", bot.getMessages().get(2));
 
-    /**
-     * Если пользователь присылает команду /notify, тогда его состояние должно стать {@link State#SET_NOTIFY_TEXT
-     * STATE.SET_NOTIFY_TEXT} и бот должен отправить сообщение с просьбой ввести текст напоминания
-     */
-    @Test
-    public void whenNotifyCommand_thenSetUserStateToNotifyTextAndAskForText() {
-        botLogic.processCommand(user, "/notify");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
-        assertEquals(State.SET_NOTIFY_TEXT, user.getState());
-        assertEquals("Введите текст напоминания", chatMessages.get(0));
-    }
-
-    /**
-     * Если во время ввода текста напоминания пользователь присылает текст напоминания, то состояние пользователя
-     * должно стать {@link State#SET_NOTIFY_DELAY STATE.SET_NOTIFY_DELAY} и бот должен отправить сообщение с просьбой
-     * ввести количество секунд напоминания
-     */
-    @Test
-    public void givenUserStateIsSetNotifyText_whenUserSendsText_thenSetUserStateToNotifyDelayAndAskForDelay() {
-        botLogic.processCommand(user, "/notify");
-        botLogic.processCommand(user, "Текст");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
-        assertEquals(State.SET_NOTIFY_DELAY, user.getState());
-        assertEquals("Через сколько секунд напомнить?", chatMessages.get(1));
-    }
-
-    /**
-     * Если во время ввода текста напоминания пользователь присылает текст напоминания, то у пользователя должно
-     * создаться напоминание
-     */
-    @Test
-    public void givenUserStateIsSetNotifyText_whenUserSendsText_thenCreateNotification() {
-        botLogic.processCommand(user, "/notify");
-        botLogic.processCommand(user, "Текст");
-
-        assertTrue(user.getNotification().isPresent());
-    }
-
-    /**
-     * Если во время ввода задержки напоминания пользователь присылает корректное число, то состояние пользователя
-     * должно стать {@link State#INIT STATE.INIT} и бот должен отправить сообщение с подтверждением создания
-     * напоминания
-     */
-    @Test
-    public void givenUserStateIsSetNotifyDelay_whenUserSendsCorrectNumber_thenSetUserStateToInitAndSendApprove() {
-        botLogic.processCommand(user, "/notify");
-        botLogic.processCommand(user, "Текст");
-
-        botLogic.processCommand(user, "2");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
+        botLogic.processCommand(user, "10");
+        assertEquals("Вы ошиблись, верный ответ: 6", bot.getMessages().get(3));
+        assertEquals(2, user.getWrongAnswerQuestions().size());
+        assertEquals("Сколько будет 2 + 2 * 2", user.getWrongAnswerQuestions().get(1).getText());
         assertEquals(State.INIT, user.getState());
-        assertEquals("Напоминание установлено", chatMessages.get(2));
+        assertEquals("Тест завершен", bot.getMessages().get(4));
     }
 
     /**
-     * Если во время ввода задержки напоминания пользователь присылает не число, то состояние пользователя
-     * должно остаться прежним и бот должен отправить сообщение с просьбой повторно ввести целое число
+     * Проверяет команду /notify и отправление напоминания вовремя
      */
     @Test
-    public void givenUserStateIsSetNotifyDelay_whenUserSendsNotANumber_thenDoNotUpdateStateAndAskDelayAgain() {
+    public void whenNotifyCommand_thenSendAtTime() throws InterruptedException {
         botLogic.processCommand(user, "/notify");
+        assertEquals(State.SET_NOTIFY_TEXT, user.getState());
+        assertEquals("Введите текст напоминания", bot.getMessages().get(0));
+
         botLogic.processCommand(user, "Текст");
+        assertEquals(State.SET_NOTIFY_DELAY, user.getState());
+        assertEquals("Через сколько секунд напомнить?", bot.getMessages().get(1));
 
         botLogic.processCommand(user, "abc");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
         assertEquals(State.SET_NOTIFY_DELAY, user.getState());
-        assertEquals("Пожалуйста, введите целое число", chatMessages.get(2));
-    }
-
-    /**
-     * Если у пользователя установлено напоминание, то бот не должен отправлять его преждевременно
-     */
-    @Test
-    public void givenUserCreatedNotification_whenItsTooEarly_thenDoNotSendANotification() {
-        botLogic.processCommand(user, "/notify");
-        botLogic.processCommand(user, "Текст");
+        assertEquals("Пожалуйста, введите целое число", bot.getMessages().get(2));
 
         botLogic.processCommand(user, "1");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
+        assertEquals(State.INIT, user.getState());
+        assertEquals("Напоминание установлено", bot.getMessages().get(3));
 
-        assertTrue(chatMessages.stream().noneMatch("Текст"::equals));
-    }
+        assertEquals(4, bot.getMessages().size());
 
-    /**
-     * Если у пользователя установлено напоминание и при этом настало время, то бот должен прислать напоминание
-     *
-     * @throws InterruptedException
-     */
-    @Test
-    public void givenUserCreatedNotification_whenTimeArrives_thenSendANotification() throws InterruptedException {
-        botLogic.processCommand(user, "/notify");
-        botLogic.processCommand(user, "Текст");
+        Thread.sleep(1500L);
 
-        botLogic.processCommand(user, "1");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
-        Thread.sleep(1500);
-        assertEquals("Сработало напоминание: 'Текст'", chatMessages.get(3));
+        assertEquals("Сработало напоминание: 'Текст'", bot.getMessages().get(4));
     }
 
     /**
@@ -228,43 +97,34 @@ public class BotLogicTest {
     @Test
     public void givenUserWithoutWrongAnswers_whenRepeat_thenSendNoQuestionsToRepeat() {
         botLogic.processCommand(user, "/repeat");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
 
-        assertEquals("Нет вопросов для повторения", chatMessages.get(0));
+        assertEquals("Нет вопросов для повторения", bot.getMessages().get(0));
     }
 
     /**
-     * Если у пользователя есть неправильно отвеченные вопросы, то при отправке пользователем команды /repeat состояние
-     * пользователя должно стать {@link State#REPEAT State.REPEAT} и бот должен прислать сообщение с первым вопросом
+     * Если у пользователя есть неправильно отвеченные вопросы, то при отправке пользователем команды /repeat бот
+     * должен начать повторение
      */
     @Test
-    public void givenUserWithWrongAnswers_whenRepeat_thenSetUserStateToRepeatAndSendFirstQuestion() {
+    public void givenUserWithWrongAnswers_whenRepeat_thenBeginRepeatSession() {
         botLogic.processCommand(user, "/test");
         botLogic.processCommand(user, "10");
-        botLogic.processCommand(user, "6");
+        botLogic.processCommand(user, "10");
 
         botLogic.processCommand(user, "/repeat");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
-
         assertEquals(State.REPEAT, user.getState());
-        assertEquals("Вычислите степень: 10^2", chatMessages.get(5));
-    }
+        assertEquals("Вычислите степень: 10^2", bot.getMessages().get(5));
 
-    /**
-     * Если пользователь отвечает на последний вопрос повторения, то состояние пользователя должно стать
-     * {@link State#INIT State.INIT} и бот должен прислать сообщение о завершении теста
-     */
-    @Test
-    public void givenUserWithWrongAnswers_whenRepeatLastQuestion_thenSetUserStateToInitAndSendStopMessage() {
-        botLogic.processCommand(user, "/test");
-        botLogic.processCommand(user, "10");
-        botLogic.processCommand(user, "6");
-
-        botLogic.processCommand(user, "/repeat");
         botLogic.processCommand(user, "100");
-        List<String> chatMessages = bot.getMessagesByChatId(user.getChatId());
+        assertEquals("Правильный ответ!", bot.getMessages().get(6));
+        assertEquals(1, user.getWrongAnswerQuestions().size());
+        assertEquals("Сколько будет 2 + 2 * 2", bot.getMessages().get(7));
 
+        botLogic.processCommand(user, "6");
+        assertEquals("Правильный ответ!", bot.getMessages().get(8));
+        assertTrue(user.getWrongAnswerQuestions().isEmpty());
         assertEquals(State.INIT, user.getState());
-        assertEquals("Тест завершен", chatMessages.get(7));
+        assertEquals("Тест завершен", bot.getMessages().get(9));
     }
+
 }
--- src/test/java/example/bot/FakeBot.java
+++ src/test/java/example/bot/FakeBot.java
@@ -1,9 +1,7 @@
 package example.bot;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Фэйковая реализация бота для тестов
@@ -13,24 +11,20 @@ public class FakeBot implements Bot {
     /**
      * Сообщения, отправленные ботом
      */
-    private final Map<Long, List<String>> messages = new HashMap<>();
+    private final List<String> messages = new ArrayList<>();
 
     @Override
     public void sendMessage(Long chatId, String message) {
-        if (!messages.containsKey(chatId)) {
-            messages.put(chatId, new ArrayList<>());
-        }
-        messages.get(chatId).add(message);
+        messages.add(message);
     }
 
     /**
-     * Возвращает сообщения, отправленные в чат с указанным ID
+     * Возвращает сообщения, отправленные ботом
      *
-     * @param chatId ID чата
-     * @return сообщения, отправленные в чат с указанным ID
+     * @return сообщения, отправленные ботом
      */
-    public List<String> getMessagesByChatId(Long chatId) {
-        return messages.get(chatId);
+    public List<String> getMessages() {
+        return messages;
     }
 
 }
